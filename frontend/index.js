b3751049332b65de4f72ca6470a5c35967d07c67
natz <natacezerbordignon@gmail.com>
2024-03-23 17:25:16 -0300

    frontend do projeto

---
+++ frontend/index.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+app.on('ready', () => {
+
+    mainWindow = new BrowserWindow({
+
+    })
+
+    mainWindow.loadURL(`File://${ __dirname }/index.html`)
+
+})
