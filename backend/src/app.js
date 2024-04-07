c94bbfb4a583ca8759f582f056e5b795c36a05bd
natz <natacezerbordignon@gmail.com>
2024-04-07 02:13:03 -0300

    server da aplicação

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 const dotenv = require ('dotenv')
 const express = require ('express')
+const jwt = require('jsonwebtoken')
+const route = require('./routes/routes')
+const bodyParser = require('body-parser')
+
 require("dotenv").config()
 
 const app = express()
 const PORT = process.env.PORT || 3333
 
+app.use(bodyParser.json())
+app.use(route)
 app.listen(3333, () => {
     console.log(`Server conectado na porta ${PORT}`)
 })
