596fea8600f2861d1bc28759dc7000e62b60c168
natz <natacezerbordignon@gmail.com>
2024-04-07 02:09:18 -0300

    validações da aplicação

---
+++ backend/src/validation/authValidator.js
@@ -0,0 +1,72 @@
+const checkSchema = require('express-validator')
+const allowedDomains = ['gmail.com', 'hotmail.com', 'yahoo.com'];
+
+const isValidEmailDomain = (value) => {
+    const emailDomain = value.split('@')[1];
+    return allowedDomains.includes(emailDomain);
+};
+
+module.exports = {
+    registerSchema: checkSchema.checkSchema({
+        name: {
+            isLength: {
+                options: {
+                    min: 6,
+                    max: 50
+                }
+            },
+            errorMessage: "O nome não condiz com os limites de tamanho!"
+        },
+
+        email: {
+            isLength: {
+                options: {
+                    min: 7,
+                    max: 50
+                }
+            },
+            custom: {
+                options: (value) => {
+                    if(!isValidEmailDomain(value)){
+                        throw new Error("O dominio do email não é permitido!")
+                    }
+                    return true
+                }
+            },
+            isEmail: true,
+            errorMessage: "Email inválido!"
+        },
+
+        password: {
+            isLength: {
+                options: {
+                    min: 8,
+                    max: 100
+                }
+            }
+        }
+    }),
+
+    loginSchema: checkSchema.checkSchema({
+        email: {
+            isLength: {
+                options: {
+                    min: 7,
+                    max: 50
+                }
+            },
+
+            isEmail: true,
+            errorMessage: "Email inválido para login!"
+        },
+
+        password: {
+            isLength: {
+                options: {
+                    min: 8,
+                    max: 100
+                }
+            }
+        }
+    })
+}
