96de90c3bcffe5f38b52c944b9479feacfd7d252
unknown <natacezerbordignon@gmail.com>
2024-04-17 21:47:35 -0300

    rotas ainda em andamento

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express')
 const authValidator = require('../validation/authValidator')
 const authController = require('../controllers/authController')
+const adminValidator = require('../validation/adminValidator')
+const adminController = require('../controllers/adminController')
 
 const route = Router()
 
@@ -11,9 +13,9 @@ route.post("/admin/user/create", adminValidator.createUserSchema, adminControlle
 route.post("/admin/user/:id/update", adminValidator.updateUserSchema, adminController.updateUser)
 route.delete("/admin/user/:id/delete", adminValidator.deleteUserSchema, adminController.deleteUser)
 
-route.put("/provider-tool/services/create", providerValidator.createSchema, providerController.putUser)
-route.post("/provider-tool/services/:id/update", provideValidator.updateSchema, provideController.postUser)
-route.delete("/provider-tool/services/:id/delete", provideValidator.deleteSchema, provideController.deleteUser)
+/*route.put("/provider-tool/services/create", providerValidator.createSchema, providerController.putUser)
+route.post("/provider-tool/services/:id/update", providerValidator.updateSchema, providerController.postUser)
+route.delete("/provider-tool/services/:id/delete", providerValidator.deleteSchema, providerController.deleteUser)
 
 route.get("/providers/:id/services", clientConntroller.getProviderServicesById)
 route.get("/locations", clientController.getLocations)
@@ -21,5 +23,5 @@ route.get("/locations", clientController.getLocations)
 route.post("/services/:id/post-comment", clientValidator.postSchema, clientController.postUser)
 
 route.delete("/services/:id/delete-comment",clientValidator.deleteSchema, clientController.deleteUser)
-
+*/
 module.exports = route
