78929551ab2335db6c2053559e01a234ee6d491e
natzaum2 <natacezerbordignon@gmail.com>
2024-06-02 01:46:20 -0300

    alterado configuração de autenticação de usuários com jwt

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -1,10 +1,11 @@
 const passport = require('passport');
 const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt');
-const { User } = require('../models/userModel');
+const User = require('../models/userModel');
 const JWT = require('jsonwebtoken');
 require('dotenv').config();
 
-const notAuthorized = { message: 'not authorized', status: 401 };
+const notAuthorized = { message: 'Não autorizado', status: 401 };
+const mustbeAdmin = { message: 'O usuário não possui o cargo administrativo', status: 401 };
 
 const options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -13,13 +14,14 @@ const options = {
 
 passport.use(new JWTStrategy(options, async (payload, done) => {
     try {
-        const user = await User.findByPk(payload.id);
+        const user = await User.getUserByEmail(payload.email);
         if (user) {
             return done(null, user);
         } else {
             return done(notAuthorized, false);
         }
     } catch (error) {
+        console.log(error)
         return done(error, false);
     }
 }));
@@ -28,21 +30,23 @@ const generatejwt = (data) => {
     return JWT.sign(data, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
 };
 
-const isAdmin = (jwt) => {
+const isAdmin = (req, res, next) => {
+    let jwt = req.headers.authorization 
     const bearerRemover = jwt.replace('Bearer ', '');
     try {
         const decodedToken = JWT.verify(bearerRemover, process.env.JWT_SECRET_KEY);
-        if (decodedToken.isAdmin === true) {
-            return true;
-        }
-        return false;
+        return decodedToken.isAdmin ? next() : next(mustbeAdmin);
     } catch (error) {
         console.error('Error verifying token:', error);
         return false;
     }
 };
 
-const PrivateEndpoint = (req, res, next) => {
+const onlyAuthenticate = (req, res, next) => {
+    if (['/login','/register'].includes(req.originalUrl)) {
+        return next()
+    }
+
     passport.authenticate('jwt', (err, user) => {
         if (err) {
             return next(err);
@@ -55,6 +59,6 @@ const PrivateEndpoint = (req, res, next) => {
 module.exports = {
     generatejwt,
     isAdmin,
-    PrivateEndpoint,
+    onlyAuthenticate,
     passport
 };
