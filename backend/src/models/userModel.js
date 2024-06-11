d47a8a7df84774c24843213971a970902b45162e
natzaum2 <natacezerbordignon@gmail.com>
2024-06-11 16:18:08 -0300

    correção em createUser para referenciar roles corretamente

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,13 +1,29 @@
 const db = require('../database/connection');
 
+
+const getRoleById = async (role_id) => {
+    try {
+        const role = await db.query('SELECT * FROM roles WHERE id = $1', [role_id])
+        return role.rows[0]
+    }
+    catch (err) {
+        console.log(err)
+        throw new Error(err)
+    }
+}
+
+
 const getUserByEmail = async (email) => {
     if (!email) throw new Error('Email is required')
     const getUserByEmail = await db.query('SELECT * FROM users WHERE email = $1', [email])
 
-    if(!getUserByEmail){
+    if(getUserByEmail.rows.length == 0){
         return false
     } else {
-        return getUserByEmail.rows[0]
+        let user = getUserByEmail.rows[0]
+        let role = await getRoleById(user.role_id)
+        user.role = role
+        return user
     }
 }
 
@@ -19,17 +35,28 @@ const createUser = async (userData) => {
         return false
     }
 
-    const result = await db.query('SELECT * FROM roles WHERE slug = $1', [userData.type]);
+    const result = await db.query('SELECT * FROM roles WHERE slug = $1', [userData.role]);
     const roleId = result.rows[0]; 
-
+    
+    
     const query = {
-        text: 'INSERT INTO users(name, email, password, role_id, sex , cpf, cnpj,admin) VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
+        text: 'INSERT INTO users(name, email, password, role_id, sex , cpf, cnpj, admin) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
         values: [userData.name, userData.email, userData.HashPassword, roleId.id, userData.sex, userData.CPF, userData.CNPJ, false],
     };
- 
-    await db.query(query); 
+        
+    let user = await db.query(query); 
+    let userId = user.rows[0].id;
+
+    let role = await getRoleById(roleId.id)
+
+    const insert = {
+        text: `INSERT INTO ${role.ref}(user_id, available, observations) VALUES($1, $2, $3)`,
+        values: [userId, true, 'OK'],
+    };
+
+    await db.query(insert); 
     
-}
+    }
 
 
 module.exports = {
