f9a6b2b4815f7e01953254c58305015931cf2778
natzaum2 <natacezerbordignon@gmail.com>
2024-06-14 16:41:21 -0300

    validator não permite que a message seja vazia

--- backend/src/validation/messageValidator.js
+++ backend/src/validation/messageValidator.js
@@ -3,8 +3,8 @@ const checkSchema = require('express-validator')
 module.exports = {
     messageValidator: checkSchema.checkSchema({
         message: {
+            notEmpty: true,
             isLength: {
-                min: 1,
                 max: 5000,
             },
             errorMessage: "content deve ser menor do que 5000 caracteres e maior do que 1 caractere"
